df15ca8785cbd34176b71fee3ca6d48a573ab81b
alpakas96 <jhurleyfilm@gmail.com>
2023-02-09 14:20:49 -0500

    working on modal

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,39 @@
+// This imports the event from the homepage. Update later: 
+// import { event } from /homepage.jsx
+import { React } from "react";
+
+// data structure:
+// image 
+// description 
+// location 
+// likes
+// comments
+
+//selectedPost is a state type property in each of the posts when they are put in the feed. 
+const Modal = ({ currentPost, modalOpen }) => {
+    
+    const handleClose = () => {
+      setIsOpen(false);
+    };
+  
+    return (
+        <>
+
+        </>
+    )
+}
+
+// write post component 
+// write .map function to populate home.jsx with post components 
+// initialize modalOpen and setModalOpan on home.jsx 
+// initialize currentPost and setCurrentPost on home.jsx 
+// write setModalOpen function on home.jsx 
+// write {modalOpen && <Modal currentPost={currentPost}>} on home.jsx 
+
+
+
+
+
+
+
+
