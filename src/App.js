1589ca2fdaa0b8de476e945e0c891a3cf26d0638
bryxnt1 <bryantperez56@gmail.com>
2023-02-10 15:08:36 -0500

    major updates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 import Hamburger from './components/HamburgerMenu/Hamburger.jsx';
-import Home from './screens/Home/Home.jsx';
+import Home from './screens/Home/Home.jsx'
 import Signin from './screens/Signin/Signin.jsx';
 
 
