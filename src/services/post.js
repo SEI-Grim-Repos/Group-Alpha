cc9bd26929240b9ce9e2a7b88da586640b45ee7a
muhammadwarrad <moe_warrad@yahoo.com>
2023-02-10 11:06:35 -0500

    fixed ports for user

--- src/services/post.js
+++ src/services/post.js
@@ -4,7 +4,7 @@ import api from "./apiConfig.js";
 
 export const getPosts = async () => {
   try {
-    const response = await api.get("/posts");
+    const response = await api.get("/post");
     return response.data;
   } catch (error) {
     throw error;
