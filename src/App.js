fd72519ee26762f7e0ef01e0d3642defd7333162
ksooooza <ksoooza@gmail.com>
2023-02-13 09:23:37 -0500

    Some routing up and going with some changes to the hamburger menu

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Hamburger from './components/HamburgerMenu/Hamburger.jsx';
-import Home from './screens/Home/Home.jsx'
+import Home from './screens/Home/Home.jsx';
+import Signout from './screens/Signout/Signout.jsx'
+import Signin from './screens/Signin/Signin.jsx'
 
 
 function App() {
   return (
     <div className="App">
-      <Hamburger />
+      <button><Hamburger /> Start</button>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Signin />} />
+          <Route path="/all-posts" element={<Home />} />
+          <Route path="/sign-out" element={<Signout />} />
         </Routes>
     </div>
   );
