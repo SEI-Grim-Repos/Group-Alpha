7edbe911f08187154b2d8759a580df3f198eb523
bryxnt1 <bryantperez56@gmail.com>
2023-02-13 09:46:03 -0500

    updates

--- src/screens/Signin/Signin.jsx
+++ src/screens/Signin/Signin.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Signin() {
+  const navigate = useNavigate();
   const [user, setUser] = useState({
     username: "",
     password: "",
