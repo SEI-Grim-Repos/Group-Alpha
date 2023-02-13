945076a8fe5a4a8cf1b3e0c7d2affe1ace9ce8d0
bryxnt1 <bryantperez56@gmail.com>
2023-02-13 09:35:32 -0500

    add signin route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Hamburger from './components/HamburgerMenu/Hamburger.jsx';
 import Home from './screens/Home/Home.jsx'
+import Signin from './screens/Signin/Signin.jsx';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Hamburger />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<Signin />} />
         </Routes>
     </div>
   );
