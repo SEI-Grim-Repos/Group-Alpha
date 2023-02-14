a2b6a45f69e3c892e82e858b2fee87a4e7f7cc6a
Kan5661 <linkan0718@gmail.com>
2023-02-14 15:18:36 -0500

    center post modal

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import Hamburger from '../../components/HamburgerMenu/Hamburger.jsx';
 import { getPosts } from '../../services/post.js';
 import Modal from '../../components/Modal/Modal.jsx'
-import Hamburger from '../../components/HamburgerMenu/Hamburger.jsx'
 import "./home.css"
 
 const Home = () => {
