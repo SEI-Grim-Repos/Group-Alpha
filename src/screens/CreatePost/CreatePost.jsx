ad224a26f11d91619de7df5a52cc1f86c4881236
orpichardo <orlandopichardo809@gmail.com>
2023-02-16 16:57:03 -0500

    brought back code for id and likes

--- src/screens/CreatePost/CreatePost.jsx
+++ src/screens/CreatePost/CreatePost.jsx
@@ -3,28 +3,23 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { COUNTRIES_LIST } from './Countries.js';
 import "./CreatePost.css"
-
 function NewPost(){
     const [countries,setcontries] = useState()
     const [ post, setPost ] = useState({
-        
+        user_id: "",
         image: "",
         title: "",
         body: "",
         location: "",
         likes: ""
     })
-
-
 let navigate = useNavigate();
-
 const handleSubmit = async (e) => {
     e.preventDefault();
     const response = await createPost(post);
     navigate(`/`, { replace: true })
     console.log(response)
 };
-
 const handleChange = (e) => {
     const { value, name } = e.target;
     setPost((prev) => ({
@@ -32,59 +27,53 @@ const handleChange = (e) => {
         [name]: value
     }))
 }
-
-
 return (
-
     <div className='postbody' >
-
     <div className='bodycreate'>
         <h1 className='title'>FoodiePost</h1>
         <form className="create-form" onSubmit={handleSubmit} /*style={{flexDirection: 'column'}}*/>
-           
-           
+            <input type="text"
+            placeholder="Enter User ID"
+            name="user_id"
+            value={post.user_id}
+            onChange={handleChange}></input>
             <input type="text"
             placeholder="Enter image URL"
             name="image"
             value={post.image}
             onChange={handleChange}></input>
-            
             <input type="text"
             placeholder="Enter title"
             name="title"
             value={post.title}
             onChange={handleChange}></input>
-            
             <input type="text"
             placeholder="Enter body..."
             name="body"
             value={post.body}
             onChange={handleChange}></input>
-{/*             
+{/*
             <input type="text"
             placeholder="Enter location"
             name="location"
             value={post.location}
             onChange={handleChange}></input> */}
-         
               <select name="location" onChange={handleChange} id = 'location' >
                 {COUNTRIES_LIST.map((cont, idx) => {
-                    
                     return(
                     <option value={cont[0]}>{cont[1]}</option>
                 )})}
-            </select>   
-
-           
+            </select>
+            <input type="text"
+            placeholder="Enter likes"
+            name="likes"
+            value={post.likes}
+            onChange={handleChange}></input>
            <br></br>
             <button type="submit">Create Post</button>
         </form>
-       
     </div>
             </div>
 )
 }
-
-
-
 export default NewPost;
